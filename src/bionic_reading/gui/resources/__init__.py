629ba658f68f279d19da72baf6c629a34fe51e70
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-06-02 00:10:22 +0900

    move resources inside so src/addon works as is when soft-linked into addon dir

---
+++ src/bionic_reading/gui/resources/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+from aqt.qt import QDir
+
+def initialize_qt_resources():
+    QDir.addSearchPath("AnKing", str(Path(__file__).parent / "AnKing"))
+
+initialize_qt_resources()
