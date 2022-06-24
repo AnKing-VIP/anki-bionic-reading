340aba4f448a20415c6138b5ab25065518c8259e
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-06-24 16:15:22 +0900

    fix Anking Menu

--- src/bionic_reading/gui/menu.py
+++ src/bionic_reading/gui/menu.py
@@ -1,10 +1,10 @@
 from aqt.qt import QAction
 
 from .anking_menu import get_anking_menu
-
+from ..config import conf
 
 def setup_menu() -> None:
     menu = get_anking_menu()
-    a = QAction("Menu Name", menu)
+    a = QAction("Bionic Reading", menu)
     menu.addAction(a)
-    a.triggered.connect(lambda _: print("abc"))
+    a.triggered.connect(conf.open_config)
